696ae35c8fb3e65849235bc5f3b0a543a54640e9
Gregory Palacios <gregpalacios26@gmail.com>
2020-12-19 22:06:13 -0500

    Primer commit PWA

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+// Guardar en el cache dinamico
+function actualizaCacheDinamico(dynamicCache, req, res) {
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
